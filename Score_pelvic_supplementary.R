33adbb1fcbc1867a7390b57522ec5f847c5d5f9a
Adrià Cruells <148210077+ADRS94@users.noreply.github.com>
2024-07-16 23:59:58 +0200

    Update and rename Final_Code_urinary.R to Score_pelvic_supplementary.R

--- Score_pelvic_supplementary.R
+++ Score_pelvic_supplementary.R
@@ -4,18 +4,15 @@
 ###     Functions Main file exam -> Created by Adrià         ######
 ###  Linkedin: https://www.linkedin.com/in/adria-cruells/    ######
 ###  ORCID: https://orcid.org/0000-0002-1179-7997            ######
-###  Github:    Working on it                                ######
+###  Github:    https://github.com/ADRS94                    ######
 ###################################################################
 ###################################################################
 
-rm(list=ls())
+r
 
-######## Enter the phyloseq object, set working directory and chargue libraries
-
-
-setwd("C:/Users/Adrià/Desktop/Trabajo/Microbioma R") ###### put the directory were you work
-Vaginal <- readRDS(file = "C:/Users/Adrià/Desktop/Trabajo/Microbioma R/Clean_codes/Vaginal2.rds")  #Enter directory of a created phytloseq( *:RData)
-Gut <- readRDS(file = "C:/Users/Adrià/Desktop/Trabajo/Microbioma R/Clean_codes/Gut.rds")  #Enter directory of a created phytloseq( *:RData)    subset_taxa(!Genus == "Homo")%>% for vaginal and fecal microbiome
+setwd("") ###### put the directory were you work
+Vaginal <- readRDS(file = "")  #Enter directory of a created phytloseq( *:RData)
+Gut <- readRDS(file = "")  #Enter directory of a created phytloseq( *:RData)    subset_taxa(!Genus == "Homo")%>% for vaginal and fecal microbiome
 
 ##########
 ####Filter
@@ -32,12 +29,6 @@ Gut <- Gut %>%
   subset_taxa(!grepl("unk",Genus)) %>%
   subset_samples(Pacient=="Yes")
 
-
-# > table(sample_data(Vaginal)$Score_pelvic)
-# 
-# 0  1  2  3  4  5  6  7  8 10 11 12 13 14 15 16 17 18 19 21 22 24 25 27 28 29 31 32 38 39 44 
-# 1  1  1  1  4  4  2  2  2  2  1  1  4  3  1  1  2  1  1  1  1  1  1  1  1  1  1  1  1  1  1 
-####Vamos a generar dos grupos, aquellas muestras con una puntuacion menor a 10 i con puntuacion major a 10 (20 menos, 27 mas)
 
 
 ################
@@ -59,38 +50,6 @@ for (analysis in list.files(paste0(getwd(),"/Clean_codes/"), pattern = ".R")) { 
 ##################Fecal###################
 ##########################################
 
-
-
-############
-##### Especific taxa composition function
-############
-
-# Organism <- c("Escherichia coli","Staphylococcus","Klebsiella","Proteus")
-# 
-# 
-# All_models <- data.frame()
-# 
-# 
-# 
-# for (i in Organism) {
-#   
-#   for (Micro in unique(rownames(otu_table(Gut))[grep(pattern = i,rownames(otu_table(Gut)))])) {
-#    Model_linear <-  Taxa_function_lineal(Physeq = Gut,Adjformula = Adjformula,Microorganism = Micro,
-#                   Analysis_name = Analysis_name,
-#                   Results_directory = Results_directory
-#                     )
-#    
-#    Linear_effect <- as.data.frame(coef(summary(Model_linear)))
-#    
-#    All_models <- rbind(All_models,cbind(paste(strsplit(Micro, ";")[[1]][7]),Linear_effect[rownames(Linear_effect) == Grup_variable,]))
-#    
-#    
-#   }
-# }
-# 
-# ###Make the forest plot
-# colnames(All_models) <- c("Organism","Estimate","SE","Z_value","P_value")
-# write.csv(All_models,file = paste0(Results_directory,"/All_Models",Analysis_name,".csv"))
 
 
 ############
@@ -150,38 +109,7 @@ plot_complete <- ggarrange(plotlist = Plot_list_lfc,labels = c("Order" ,  "Famil
 ##################Vaginal#################
 ##########################################
 
-Analysis_name <- "First_try_Vaginal" ###Group analysis name
-
-
-############
-##### Plot composition function
-############
-
-
-# Composition_function(Vaginal, ####Physeq
-#                      Grup_variable, ###Group variable
-#                      "Genus", ###Tax graf
-#                      Analysis_name, ###File_names
-#                      Results_directory) ## ## Result of file names
-
-
-############
-##### Especific taxa composition function
-############
-
-# Organism <- c("Escherichia coli","Enterococcus","Staphylococcus","Klebsiella","Proteus")
-# 
-# 
-# 
-# for (i in Organism) {
-#   
-#   for (Micro in unique(rownames(otu_table(Vaginal))[grep(pattern = i,rownames(otu_table(Vaginal)))])) {
-#     Taxa_function(Physeq = Vaginal,Grup_variable = Grup_variable,Microorganism = Micro,
-#                   Analysis_name = Analysis_name,
-#                   Results_directory = Results_directory
-#     )
-#   }
-# }
+Analysis_name <- "Score_pelvic_vaginal" ###Group analysis name
 
 
 ############
@@ -267,24 +195,3 @@ summary(model)
 
 
 plot(model)
-
-############
-##### Differential Secom function
-############
-
-##### Select the diferential taxa and observe the effect in the other biome.
-sigtaxa_vagi <- paste0("Vaginal - ",vagilfc$taxon[vagilfc$diff_PacientYes == "TRUE"])
-sigtaxa_feces <- paste0("Fecal - ",Feceslfc$taxon[Feceslfc$diff_PacientYes == "TRUE"])
-
-sigtaxa <- c(sigtaxa_vagi,sigtaxa_feces)
-#####Obtain the correlation matrix (the retunr of Both_microbiomes function)
-
-cor_matrix <- as.data.frame(Differential_ecosistem(Gut,Vaginal,"Genus"))
-
-corr_sig <- cor_matrix[grep(paste(sigtaxa,collapse="|"),rownames(cor_matrix)),]
-
-
-################################ Obtain the correlation only of the Ancombc taxa
-
-
-
